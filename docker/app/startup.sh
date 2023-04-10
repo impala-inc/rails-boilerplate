f266387067822fdbe5b1cf27ec599a6cd73698db
Shin Morichika <ofl@covered.jp>
2023-04-11 07:08:08 +0900

    Fixed to install gem into vendor/bundle

--- docker/app/startup.sh
+++ docker/app/startup.sh
@@ -3,14 +3,20 @@
 if [ -f Gemfile ] ; then
   bundle install
 else
+  sudo chown -R 1000:1000 ./vendor/bundle
+
+  bundle init
+  bundle config --local path vendor/bundle
   rails new . --force \
               --database=postgresql \
-              --skip-git --css=tailwind \
+              --skip-git \
+              --css=tailwind \
               --skip-jbuilder \
               --skip-action-mailer \
               --skip-test \
               --skip-active-storage \
               --skip-action-text
 fi
-bin/setup
-bin/dev
+sudo chown -R 1000:1000 ./tmp
+./bin/setup
+./bin/dev
