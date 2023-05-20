1b10f0fbcdfddf971984cd0fd859c38267b606b6
Shin Morichika <ofl@covered.jp>
2023-05-20 09:55:54 +0900

    fix: Fixed permission error on build from Gemfile

--- docker/app/startup.sh
+++ docker/app/startup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -e
 
+sudo chown -R 1000:1000 .
+
+bundle config --local path vendor/bundle
+
 if [ -f Gemfile ] ; then
   bundle install
 else
-  sudo chown -R 1000:1000 .
-
   bundle init
-  bundle config --local path vendor/bundle
   rails new . --force \
               --database=postgresql \
               --skip-git \
